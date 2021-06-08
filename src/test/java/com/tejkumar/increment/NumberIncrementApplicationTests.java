8bdf3bd98aef23c739226cada6d576c1c1f55ee4
Tejkumar17 <85554711+Tejkumar17@users.noreply.github.com>
2021-06-08 23:25:49 +0530

    Add files via upload

---
+++ src/test/java/com/tejkumar/increment/NumberIncrementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejkumar.increment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberIncrementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
